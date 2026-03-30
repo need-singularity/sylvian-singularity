2ac846a8da9e3dc84a05d186537cf8948efaafcc
dancinlife <nerve011235@gmail.com>
2026-03-31 06:24:47 +0900

    fix: remove tecsrs/target from tracking, add to .gitignore

--- tecsrs/target/release/build/libc-d3a1e35b13f2140e/build_script_build-d3a1e35b13f2140e.d
+++
@@ -1,5 +0,0 @@
-/Users/ghost/Dev/TECS-L/tecsrs/target/release/build/libc-d3a1e35b13f2140e/build_script_build-d3a1e35b13f2140e.d: /Users/ghost/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.183/build.rs
-
-/Users/ghost/Dev/TECS-L/tecsrs/target/release/build/libc-d3a1e35b13f2140e/build_script_build-d3a1e35b13f2140e: /Users/ghost/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.183/build.rs
-
-/Users/ghost/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.183/build.rs:
--- tecsrs/target/release/build/pyo3-ffi-1284a20797efd801/build_script_build-1284a20797efd801.d
+++
@@ -1,5 +0,0 @@
-/Users/ghost/Dev/TECS-L/tecsrs/target/release/build/pyo3-ffi-1284a20797efd801/build_script_build-1284a20797efd801.d: /Users/ghost/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pyo3-ffi-0.28.2/build.rs
-
-/Users/ghost/Dev/TECS-L/tecsrs/target/release/build/pyo3-ffi-1284a20797efd801/build_script_build-1284a20797efd801: /Users/ghost/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pyo3-ffi-0.28.2/build.rs
-
-/Users/ghost/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pyo3-ffi-0.28.2/build.rs:
